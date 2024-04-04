992df70fc77748154d6da274541a419e2ecaba74
Andrei Warkentin <andrei.warkentin@intel.com>
2024-04-04 00:55:39 -0500

    example_kernel: early work on MMU
    Actual page table setup is done. Leaving SATP and testing for another day.
    
    Signed-off-by: Andrei Warkentin <andrei.warkentin@intel.com>

--- example_kernels/riscv64/kernel.h
+++ example_kernels/riscv64/kernel.h
@@ -6,7 +6,7 @@
 
 #include <inttypes.h>
 
-#define PAGE_SIZE    4096
+#define LINK_ADDRESS 0x80300000
 #define STACK_PAGES  1
 /*
  * Mapping the kernel itself using 2MB megapage. Assume
@@ -16,15 +16,44 @@
  */
 #define EARLY_PAGES  4
 
-#define LINK_ADDRESS 0x80300000
-
 #define R_RISCV_RELATIVE 0x3
-
 typedef struct elf64_rela {
   uint64_t r_offset;
   uint64_t r_info;
   uint64_t r_addend;
 } elf64_rela;
+
+/*
+ * We can have up to 5 levels on RISC-V, let's call
+ * these L0 (which contains entries mapping 4KiB pages) to
+ * L4 (which contains entries mapping 256TiB petapages).
+ *
+ * Each page level is indexed by PG_TAB_SHIFT (i.e. 512 entries).
+ */
+#define PAGE_SHIFT                 12
+#define PAGE_SIZE                  (1UL << PAGE_SHIFT)
+#define PAGE_MASK                  (PAGE_SIZE - 1)
+#define PAGE_MASK_INVERSE          (~PAGE_MASK)
+#define PG_TAB_SHIFT               9
+#define PG_TAB_ENTRIES             (1 << PG_TAB_SHIFT)
+#define PG_TAB_MASK                (PG_TAB_ENTRIES - 1)
+#define PG_LEVEL_SHIFT(level)      (((level) * PG_TAB_SHIFT) + PAGE_SHIFT)
+#define PG_LEVEL_SIZE(level)       (1UL << PG_LEVEL_SHIFT(level))
+#define PG_LEVEL0_SHIFT            PG_LEVEL_SHIFT(0)
+#define PG_LEVEL1_SHIFT            PG_LEVEL_SHIFT(1)
+#define PG_LEVEL2_SHIFT            PG_LEVEL_SHIFT(2)
+#define PG_LEVEL3_SHIFT            PG_LEVEL_SHIFT(3)
+#define PG_LEVEL4_SHIFT            PG_LEVEL_SHIFT(4)
+#define PG_LEVEL_OFFSET(level, va) ((va) >> PG_LEVEL_SHIFT(level)) & PG_TAB_MASK)
+#define PG_ENT_PPN_MASK            0x3FFFFFFFFFFC00
+/*
+ * PPN is in bits [10:53].
+ */
+#define PG_ENT_PPN_SHIFT           2
+#define PG_ENT_VALID               (1UL << 0)
+#define PG_ENT_READ                (1UL << 1)
+#define PG_ENT_WRITE               (1UL << 2)
+#define PG_ENT_EXECUTE             (1UL << 3)
 
 void sbi_putchar(char ch);
 int printf(char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
